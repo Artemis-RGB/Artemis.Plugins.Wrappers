a6369d52675cc6edeea6b928b653d53fda5890ba
Diogo Trindade <diogotr7@gmail.com>
2023-03-27 22:17:37 +0100

    Fixed prerequisites

---
+++ src/LightFx/Artemis.Plugins.Wrappers.LightFx/Prerequisites/LightFxWrapperPrerequisite.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Artemis.Core;
+
+namespace Artemis.Plugins.Wrappers.LightFx.Prerequisites;
+
+public class LightFxWrapperPrerequisite : PluginPrerequisite
+{
+    private const string DLL_NAME = "Artemis.Wrappers.LightFx.dll";
+    private const string DLL_NAME_SYSTEM = "LightFX.dll";
+    private readonly string _dll32;
+    private readonly string _dll64;
+    private readonly string _systemDll32;
+    private readonly string _systemDll64;
+
+    public LightFxWrapperPrerequisite(Plugin plugin)
+    {
+        _systemDll64 = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), DLL_NAME_SYSTEM);
+        _systemDll32 = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.SystemX86), DLL_NAME_SYSTEM);
+        _dll64 = plugin.ResolveRelativePath(Path.Combine("x64", DLL_NAME));
+        _dll32 = plugin.ResolveRelativePath(Path.Combine("x86", DLL_NAME));
+
+        InstallActions = new List<PluginPrerequisiteAction>
+        {
+            new RunInlinePowerShellAction("Copy 64 bit dll", $"Copy-Item -Path {_dll64} -Destination {_systemDll64}",
+                true),
+            new RunInlinePowerShellAction("Copy 32 bit dll", $"Copy-Item -Path {_dll32} -Destination {_systemDll32}",
+                true)
+        };
+        UninstallActions = new List<PluginPrerequisiteAction>();
+    }
+
+    /// <inheritdoc />
+    public override string Name => "Place wrapper Dlls";
+
+    /// <inheritdoc />
+    public override string Description => "This will copy the needed files to capture LightFX lighting";
+
+    /// <inheritdoc />
+    public override List<PluginPrerequisiteAction> InstallActions { get; }
+
+    /// <inheritdoc />
+    public override List<PluginPrerequisiteAction> UninstallActions { get; }
+
+    /// <inheritdoc />
+    public override bool IsMet()
+    {
+        return IsDllValid(_systemDll64, _dll64)
+               && IsDllValid(_systemDll32, _dll32);
+    }
+
+    private static bool IsDllValid(string fileName, string compareTo)
+    {
+        if (!File.Exists(fileName)) return false;
+
+        var info = FileVersionInfo.GetVersionInfo(fileName);
+        var desiredInfo = FileVersionInfo.GetVersionInfo(compareTo);
+
+        return info?.CompanyName == desiredInfo?.CompanyName &&
+               info?.ProductName == desiredInfo?.ProductName &&
+               info?.ProductVersion == desiredInfo?.ProductVersion;
+    }
+}
--- src/Logitech/Artemis.Plugins.Wrappers.Logitech/Prerequisites/LogitechWrapperPrerequisite.cs
+++ src/Logitech/Artemis.Plugins.Wrappers.Logitech/Prerequisites/LogitechWrapperPrerequisite.cs
@@ -1,56 +1,62 @@
-﻿using Artemis.Core;
-using Microsoft.Win32;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
+using Artemis.Core;
+using Microsoft.Win32;
 
-namespace Artemis.Plugins.Wrappers.Logitech.Prerequisites
+namespace Artemis.Plugins.Wrappers.Logitech.Prerequisites;
+
+internal class LogitechWrapperPrerequisite : PluginPrerequisite
 {
-    internal class LogitechWrapperPrerequisite : PluginPrerequisite
+    private const string DLL_NAME = "LogitechLed.dll";
+
+    private const string REGISTRY_PATH_64 =
+        "SOFTWARE\\Classes\\CLSID\\{a6519e67-7632-4375-afdf-caa889744403}\\ServerBinary";
+
+    private const string REGISTRY_PATH_32 =
+        "SOFTWARE\\Classes\\WOW6432Node\\CLSID\\{a6519e67-7632-4375-afdf-caa889744403}\\ServerBinary";
+
+    private readonly string _dllPath32;
+    private readonly string _dllPath64;
+
+    public LogitechWrapperPrerequisite(Plugin plugin)
     {
-        public override string Name => "Logitech wrapper registry patch";
+        _dllPath64 = Path.Combine(plugin.Directory.FullName, "x64", DLL_NAME);
+        _dllPath32 = Path.Combine(plugin.Directory.FullName, "x86", DLL_NAME);
 
-        public override string Description => "This registry patch makes games send their lighting to Artemis instead of LGS or LGHUB";
+        var patchScript = plugin.ResolveRelativePath("Scripts\\patch-registry.ps1");
+        var unpatchScript = plugin.ResolveRelativePath("Scripts\\unpatch-registry.ps1");
 
-        public override List<PluginPrerequisiteAction> InstallActions { get; }
-
-        public override List<PluginPrerequisiteAction> UninstallActions { get; }
-
-        public override bool IsMet()
+        InstallActions = new List<PluginPrerequisiteAction>
         {
-            using RegistryKey key64 = Registry.LocalMachine.OpenSubKey(REGISTRY_PATH_64);
-            using RegistryKey key32 = Registry.LocalMachine.OpenSubKey(REGISTRY_PATH_32);
+            new RunPowerShellAction("Patch 64 bit registry", patchScript, true,
+                $"\"HKLM:{REGISTRY_PATH_64}\" \"{_dllPath64}\""),
+            new RunPowerShellAction("Patch 32 bit registry", patchScript, true,
+                $"\"HKLM:{REGISTRY_PATH_32}\" \"{_dllPath32}\"")
+        };
 
-            bool is64BitKeyPresent = key64?.GetValue(null)?.ToString() == _dllPath64;
-            bool is32BitKeyPresent = key32?.GetValue(null)?.ToString() == _dllPath32;
-
-            return is64BitKeyPresent && is32BitKeyPresent;
-        }
-
-        public LogitechWrapperPrerequisite(Plugin plugin)
+        UninstallActions = new List<PluginPrerequisiteAction>
         {
-            _dllPath64 = Path.Combine(plugin.Directory.FullName, "x64", DLL_NAME);
-            _dllPath32 = Path.Combine(plugin.Directory.FullName, "x86", DLL_NAME);
+            new RunPowerShellAction("Unpatch 64 bit registry", unpatchScript, true, $"\"HKLM:{REGISTRY_PATH_64}\""),
+            new RunPowerShellAction("Unpatch 32 bit registry", unpatchScript, true, $"\"HKLM:{REGISTRY_PATH_32}\"")
+        };
+    }
 
-            var patchScript = plugin.ResolveRelativePath("Scripts\\patch-registry.ps1");
-            var unpatchScript = plugin.ResolveRelativePath("Scripts\\unpatch-registry.ps1");
-            
-            InstallActions = new List<PluginPrerequisiteAction>
-            {
-                new RunPowerShellAction("Patch 64 bit registry", patchScript, true, $"\"HKLM:{REGISTRY_PATH_64}\" \"{_dllPath64}\""),
-                new RunPowerShellAction("Patch 32 bit registry", patchScript, true, $"\"HKLM:{REGISTRY_PATH_32}\" \"{_dllPath32}\"")
-            };
+    public override string Name => "Logitech wrapper registry patch";
 
-            UninstallActions = new List<PluginPrerequisiteAction>
-            {
-                new RunPowerShellAction("Patch 64 bit registry", unpatchScript, true, $"\"HKLM:{REGISTRY_PATH_64}\""),
-                new RunPowerShellAction("Patch 32 bit registry", unpatchScript, true, $"\"HKLM:{REGISTRY_PATH_32}\"")
-            };
-        }
+    public override string Description => "This registry patch makes games send their lighting to Artemis instead of LGS or LGHUB";
 
-        private const string DLL_NAME = "LogitechLed.dll";
-        private const string REGISTRY_PATH_64 = "SOFTWARE\\Classes\\CLSID\\{a6519e67-7632-4375-afdf-caa889744403}\\ServerBinary";
-        private const string REGISTRY_PATH_32 = "SOFTWARE\\Classes\\WOW6432Node\\CLSID\\{a6519e67-7632-4375-afdf-caa889744403}\\ServerBinary";
-        private readonly string _dllPath64;
-        private readonly string _dllPath32;
+    public override List<PluginPrerequisiteAction> InstallActions { get; }
+
+    public override List<PluginPrerequisiteAction> UninstallActions { get; }
+
+    public override bool IsMet()
+    {
+        using var key64 = Registry.LocalMachine.OpenSubKey(REGISTRY_PATH_64);
+        using var key32 = Registry.LocalMachine.OpenSubKey(REGISTRY_PATH_32);
+
+        var is64BitKeyPresent = key64?.GetValue(null)?.ToString() == _dllPath64;
+        var is32BitKeyPresent = key32?.GetValue(null)?.ToString() == _dllPath32;
+
+        return is64BitKeyPresent && is32BitKeyPresent;
     }
 }
